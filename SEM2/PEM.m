7b0f6f036e9f34c7e7e41ed1509326dd21723c8e
svrcinon <91096757+svrcinon@users.noreply.github.com>
2023-07-12 15:48:44 +0200

    data

---
+++ SEM2/PEM.m
@@ -0,0 +1,40 @@
+% PEM a rekurzivní PEM - upravit kod 
+
+clear all
+load("data.mat")
+y = data;
+%%
+theta=[1;1;1];
+thetaN=zeros(3,1);
+while norm(theta-thetaN)>1e-5
+    thetaN=theta;
+    sum1=zeros(3,3);
+    sum2=zeros(3,1);
+    eps=0;
+    psi=[0 0 0]';
+    for i=2:length(y)
+        sum1=sum1+psi*psi';
+        sum2=sum2+psi*eps;
+        psi=-thetaN(3)*psi+[-y(i-1) u(i-1) eps]';
+        eps=y(i)-[-y(i-1) u(i-1) eps]*thetaN;
+    end
+    theta=thetaN+inv(sum1)*sum2; 
+end
+theta
+%% rekurz
+
+th=[1;1;1];
+th_all=th;
+P=1*eye(3);
+psi=[0 0 0]';
+eps=0;
+for t=2:length(u)
+   psi=-th(3)*psi+[-y(t-1) u(t-1) eps]';
+   eps=y(t)-[-y(t-1) u(t-1) eps]*th;
+   L=P*psi/(1+psi'*P*psi);
+   th=th+L*eps;
+   P=(eye(3)-L*psi')*P;
+   th_all(:,t)=th;
+end
+th
+plot(1:N,th_all)
---
+++ SEM2/generovanidat.m
@@ -0,0 +1,41 @@
+% generujeme dat 
+
+clear; clc; close all;
+
+a = 0.6;
+b = 1; 
+c = 0.5;
+
+
+% počet dat je 501. protože t jde od nuly až do 500
+ND = 1001; 
+t = linspace(0,ND-1,ND);
+
+% definování systému 
+y = zeros(ND,1);
+y(1,1) = 0;
+
+%chyba 
+lambda = 1;
+e  =  randn(ND,1)*lambda; % bílý šum
+
+% jako šum je zvolen gauus protož je trvale budící 
+u = zeros(ND, 1);
+sigma = 1;
+u(:,1) = randn(ND, 1)*sigma; % normální
+
+    %generování 
+for i = 2:ND
+     y(i,1) = a*y(i-1,1) + b*u(i-1,1) + e(i, 1) + c*e(i-1,1);
+end
+
+p1 = figure;
+plot(t, y,'LineWidth',1.2);
+title('System');
+legend('gaussian');
+xlabel("t")
+ylabel("y(t)")
+%exportgraphics(p1,'system1.pdf')
+
+%data = y;
+%save(data)
