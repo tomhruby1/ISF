8244da0ec0fd655103066be4b38ca7309c843a3a
svrcinon <91096757+svrcinon@users.noreply.github.com>
2023-07-18 16:10:49 +0200

    bosak smazan

--- bosak/RMSE_extended.m
+++
@@ -1,6 +0,0 @@
-function [theta, P, epsilon] = RMSE_extended(fi, P_prev, theta_prev, y)
-    epsilon = y - fi'*theta_prev; %chyba odhadu     
-    L = (P_prev * fi)/(1 + fi'*P_prev*fi); %zisk
-    P = P_prev - ((P_prev*fi*fi'*P_prev)/(1 + fi'*P_prev*fi)); %nova kovariancni matice
-    theta = theta_prev + L*epsilon; %novy odhad
-end
